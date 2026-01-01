a0e33611a392aa2b3798bee80bf443e4792e0747
Ruchir Shukla <ruchir@bizzappdev.com>
2026-01-01 17:20:02 +0530

    [MIG] project_task_ancestor: Migration to 19.0

--- project_task_ancestor/__manifest__.py
+++ project_task_ancestor/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Project Task Ancestor",
-    "version": "18.0.1.0.1",
+    "version": "19.0.1.0.0",
     "category": "Services/Project",
     "website": "https://github.com/OCA/project",
     "author": "Moduon, Odoo Community Association (OCA)",
